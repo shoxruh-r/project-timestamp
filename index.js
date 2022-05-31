2692ae06aa523f3dfa0ca093d4c8dc753f4ad4ed
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-05-31 12:02:36 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// index.js
+// where your node app starts
+
+// init project
+const express = require('express')
+const app = express()
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+const cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 }))  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+
+// An empty date parameter
+app.get('/api', (req, res) => {
+  res.json({ unix: Date.now(), utc: new Date().toUTCString() })
+})
+
+
+// Request to /api/:date?
+app.get('/api/:date', (req, res) => {
+  const { date } = req.params
+  const unix = Number(date) || Number(Date.parse(date))
+
+  const utc = new Date(unix).toUTCString()
+
+  if (utc === 'Invalid Date')
+    return res.send({ error: utc })
+
+  res.json({ unix, utc })
+})
+
+
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port)
+})
